f527e516ba89b06eddd14f668d6e632c4fb873fe
Alex Pak <alexlpak@Alexs-MacBook-Pro.local>
2022-08-06 11:33:19 -0500

    removed unused components

--- src/pages/Admin/ManageRSVP.tsx
+++ src/pages/Admin/ManageRSVP.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Section from '../../components/Section';
 import { useTheme } from 'styled-components';
-import Input from '../../components/Input';
-import Form from '../../components/Form';
-import Button from '../../components/Button';
 
 const ManageRSVP: React.FC = () => {
     const theme = useTheme();
